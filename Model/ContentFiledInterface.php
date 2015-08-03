404d702704eabbbfa0f00af73a0c95eaec811dda
Artem Ostretsov <OstretsovAA@gmail.com>
2015-08-03 18:22:23 +0300

    SCI-608 #resolved

---
+++ Model/ContentFiledInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * (c) Artem Ostretsov <artem@ostretsov.ru>
+ * Created at 03.08.2015 17:24
+ */
+
+namespace SciGroup\TinymcePluploadFileManagerBundle\Model;
+
+
+interface ContentFiledInterface
+{
+}
